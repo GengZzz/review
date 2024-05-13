88ba9382a72d113a1c14b23b1d470eca756c549a
GengZzz <1362284172@qq.com>
2024-05-13 18:23:51 +0800

    item:init

---
+++ env.d.ts
@@ -0,0 +1,8 @@
+/// <reference types="vite/client" />
+// 解决引入爆红问题
+declare module '*.vue' {
+  import { DefineComponent } from 'vue'
+  const component: DefineComponent<{}, {}, any>
+  export default component
+}
+declare module 'vue-router'
